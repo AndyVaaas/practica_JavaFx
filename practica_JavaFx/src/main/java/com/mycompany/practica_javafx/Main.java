a5a9d49def03a2f1e5d22bea04f77af1427805e2
umg <umg@HUE-LAB-2D1.labhue.local>
2026-05-09 10:30:18 -0600

    Add basic UI interaction

--- practica_JavaFx/src/main/java/com/mycompany/practica_javafx/Main.java
+++ practica_JavaFx/src/main/java/com/mycompany/practica_javafx/Main.java
@@ -3,20 +3,32 @@ package com.mycompany.practica_javafx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label; 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;    
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
-    @Override
+   @Override
     public void start(Stage stage) {
-        Label label = new Label("Hola JavaFX"); 
-        Scene scene = new Scene(new VBox(label), 300, 200);
+        TextField campo = new TextField();
+        Button boton = new Button("Mostrar");
+        Label label = new Label();
+
+        // Configuramos la acción del botón
+        boton.setOnAction(e -> {
+            label.setText(campo.getText());
+        });
+
+        // Creamos el diseño y metemos los componentes
+        VBox layout = new VBox(10, campo, boton, label);
+        Scene scene = new Scene(layout, 300, 200);
+
+        // Mostramos la ventana
         stage.setScene(scene);
         stage.show();
     }
-
     // No olvides agregar el main para que corra
     public static void main(String[] args) {
         launch(args);
